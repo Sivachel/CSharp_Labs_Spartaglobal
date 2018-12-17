c070d70b2c4aa6df9ca2e05e4eef679153b2a794
Sivachel <lsivachelvan@spartaglobal.com>
2018-12-17 15:46:54 +0000

    17/12/2018

--- Labs_16_Entity/Program.cs
+++ Labs_16_Entity/Program.cs
@@ -8,13 +8,38 @@ namespace Labs_16_Entity
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-
-        }
-
         //pull in database here. DBContext is our DB object.
         static NorthwindEntities DBContext = new NorthwindEntities();
+
+        static void Main(string[] args)
+        {
+            foreach (Labs_16_Entity.Customer customer in DBContext.Customers)
+            {
+                //Console.WriteLine("{0} lives in {1}",customer.ContactName,customer.City);
+            }
+
+            var customers = from c in DBContext.Customers
+
+                            select c;
+
+            foreach(Customer c in customers)
+            {
+                //Console.WriteLine("{0} lives in {1}", c.ContactName, c.City);
+            }
+            Console.WriteLine("Enter city");
+            var inputString = Console.ReadLine();
+            var customers2 = from c in DBContext.Customers where c.City == inputString select c;
+
+            foreach(Customer c in customers2)
+            {
+                Console.WriteLine("{0} lives in {1}", c.ContactName, c.City);
+            }
+
+            var singleCustomer = DBContext.Customers.First(c => c.Country == "Finland");
+           // Console.WriteLine(singleCustomer.ContactName);
+        }
+
+       
     }
 
 }
--- Labs_17_Entitywpf/MainWindow.xaml.cs
+++ Labs_17_Entitywpf/MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace Labs_17_Entitywpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        NorthwindEntities DBcontext = new NorthwindEntities();
+
         public MainWindow()
         {
             InitializeComponent();
+            foreach(Customer c in DBcontext.Customers)
+            {
+                ListBox01.Items.Add(string.Format("{0} lives in {1}",c.ContactName,c.City));
+            }
+
+         
+        }
+        
+        private void Button01_Click(object sender, RoutedEventArgs e)
+        {
+            var customers2 = from c in DBcontext.Customers
+                             where c.City == Textbox01.Text.ToString()
+                             select c;
+
+            ListBox02.ItemsSource = customers2.ToList<Customer>();
+            ListBox02.DisplayMemberPath = "ContactName";
+        }
+
+        private void ListBox02_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Customer customers3 = ListBox02.SelectedItem as Customer;
+            ListBox03.Items.Clear();
+            //ListBox03.Items.Add(String.Format("{0} lives in {1}", customers3.ContactName, customers3.Country));
+            ListBox03.Items.Add(String.Format(" Name: {0}\n Job: {1}\n Address: {2}\n City: {3} ", customers3.ContactName, customers3.ContactTitle, customers3.Address, customers3.City));
+        }
+
+        private void Button02_Click(object sender, RoutedEventArgs e)
+        {
+            Customer customers3 = ListBox02.SelectedItem as Customer;
+            customers3.City = Textbox02.Text;
+            DBcontext.SaveChanges();
+        }
+
+        private void Button03_Click(object sender, RoutedEventArgs e)
+        {
+            int customerID = DBcontext.Customers.Count<Customer>();
+            customerID++;
+            DBcontext.Customers.Add(new Customer
+            {
+                CustomerID = customerID.ToString(),
+                CompanyName = "NULL",
+                ContactName = "NULL",
+                ContactTitle = "NULL",
+                Address = "NULL",
+                City = Textbox01.Text,
+                Region = "NULL",
+                PostalCode = "NULL",
+                Country = "NULL",
+                Phone = "NULL",
+                Fax = "NULL"
+            });
+            DBcontext.SaveChanges();
         }
     }
 }
